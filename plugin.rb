83ecf9b6e970ad5123ab675cbc2e217b028e899c
osvid <oscardavidsanchez@gmail.com>
2020-12-31 15:46:03 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -10,10 +10,9 @@ after_initialize do
     def can_delete_topic?(topic)
       alias_method :org_can_delete_topic?, :can_delete_topic?
 
-      return true if !topic.trashed? && (is_staff? || (is_my_own?(topic) || is_category_group_moderator?(topic.category)) && !topic.is_category_topic? &&
-      !Discourse.static_doc_topic_ids.include?(topic.id)
+      return true if (!topic.trashed? && (is_staff? || (is_my_own?(topic) || is_category_group_moderator?(topic.category)) && !topic.is_category_topic? &&
+      !Discourse.static_doc_topic_ids.include?(topic.id))
     end
-  end
     
    
 end
