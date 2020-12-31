41b0ff39417c8739f0fea76fe9c591da384ee8c2
osvid <oscardavidsanchez@gmail.com>
2020-12-31 15:43:13 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,17 +6,14 @@
 after_initialize do
 
   # change permissions on topic deletion
-  module TopicGuardian
+  module ::TopicGuardian
     def can_delete_topic?(topic)
-      !topic.trashed? &&
-      (is_staff? || (is_my_own?(topic) || is_category_group_moderator?(topic.category)) &&
-      !topic.is_category_topic? &&
+      alias_method :org_can_delete_topic?, :can_delete_topic?
+
+      return true if !topic.trashed? && (is_staff? || (is_my_own?(topic) || is_category_group_moderator?(topic.category)) && !topic.is_category_topic? &&
       !Discourse.static_doc_topic_ids.include?(topic.id)
     end
   end
     
-  class ::Guardian
-    prepend TopicGuardian
-  end
-    
+   
 end
